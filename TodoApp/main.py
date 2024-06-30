7186c06082bec7121714ba303c39e9bf14d7a071
Nestor Juarez <nesk.company@gmail.com>
2024-06-30 17:36:16 -0600

    Agregate validation for get todo thing by id

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,9 +1,12 @@
 from typing import Annotated
 from sqlalchemy.orm import Session 
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, Path
 import models
 from models import Todos
 from database import engine, SessionLocal
+from starlette import status
+
+
 
 app = FastAPI()
 
@@ -23,13 +26,13 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@app.get("/")
+@app.get("/",status_code=status.HTTP_200_OK)
 async def read_all(db:db_dependency):
     return db.query(Todos).all()
 
 
-@app.get("/todo/{todo_id}")
-async def read_todo(db:db_dependency, todo_id: int):
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db:db_dependency, todo_id: int = Path(gt=0)):
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
     if todo_model is not None:
         return todo_model
