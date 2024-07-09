6370cf659c357c86840c8e9fd61aaf21a09d5c79
Nestor Juarez <nesk.company@gmail.com>
2024-07-09 09:53:42 -0600

    Create new table users in database, and rename the existing

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,5 +1,20 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+
+
+
+class Users(Base):
+    __tablename__ = 'users'             #Name of the table
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+
 
 
 class Todos(Base):
@@ -12,4 +27,4 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
-
+    owner_id = Column(Integer, ForeignKey("users.id"))
