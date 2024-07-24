2b6cde17861431836add4f4543ccbc6a82591570
nestorjuarezr <nesk.company@gmail.com>
2024-07-24 16:11:53 -0600

    Configure postgresql production Database, delete SQLite database

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQALCHEMY_DATABASE_URL = 'postgresql://postgres:nescafe@localhost/TodoApplicationDatabase'
 
-engine = create_engine(SQALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SQALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
