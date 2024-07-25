3933e91b3fdaba5580e8764060be9657eba1d396
nestorjuarezr <nesk.company@gmail.com>
2024-07-24 18:25:37 -0600

    Add phone number to the info of get_user endpoint, and add the put endpoint to update phone_number

--- TodoApp/routers/users.py
+++ TodoApp/routers/users.py
@@ -57,3 +57,15 @@ async def change_password(user:user_dependency, db: db_dependency, user_verifica
     user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
     db.add(user_model)
     db.commit()
+
+
+@router.put("/phone_number/{phone_number}", status_code = status.HTTP_204_NO_CONTENT)
+async def update_phone_number(user:user_dependency, db: db_dependency, phone_number: str,user_verification: UserVerification):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authenticated Failed")
+    
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+
+    user_model.phone_number = phone_number
+    db.add(user_model)
+    db.commit()
