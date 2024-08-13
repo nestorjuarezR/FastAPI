d51854722ec0ab70f5931381d15a83cb83153b25
nestorjuarezr <nesk.company@gmail.com>
2024-08-13 13:28:51 -0600

    Add functions for test_todos file

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -65,3 +65,46 @@ def test_read_all_authenticated(test_todo):
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == [{'complete': False, 'title': 'Learn to code', 'description': 'Need to learn everyday!', 'id': 1, 'priority': 5, 'owner_id': 1}]
 
+
+
+
+def test_create_todo(test_todo):
+    request_data = {
+        'title': 'New Todo',
+        'description': 'New todo description',
+        'priority': 5,
+        'complete': False,
+    }
+    response = client.post("/todo/", json = request_data)
+    assert response.status_code == 201
+
+    db = TestingSesionLocal()
+    model = db.query(Todos).filter(Todos.id == 2).first()
+    assert model.title == request_data.get('title')
+    assert model.description == request_data.get('description')
+
+
+def test_read_one_authenticated(test_todo):
+    response = client.get("/todo/1")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {'complete': False, 
+                                'title': 'Learn to code', 
+                                'description': 'Need to learn everyday!', 
+                                'id': 1, 
+                                'priority': 5, 
+                                'owner_id': 1}
+
+
+def test_update_todo(test_todo):
+    resquest_data = {
+        'title' : 'change the title of the todo',
+        'description' : 'Need to learn',
+        'priority': 5,
+        'complete': False,
+    }
+
+    response = client.put('/todo/1', json=resquest_data)
+    assert response.status_code == 204
+    db = TestingSesionLocal()
+    model = db.query(Todos). filter(Todos.id == 1).first()
+    assert model.title == 'change the title of the todo'
